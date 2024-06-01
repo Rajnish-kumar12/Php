e093c4749afc5a321104a9a24eb112183a3c4343
Rajnish kumar <rajnishkumardb07@gmail.com>
2024-06-01 18:07:33 +0530

    add update code for switch statement

--- PhpBasics/switch.php
+++ PhpBasics/switch.php
@@ -26,3 +26,33 @@ switch ($weekday) {
     default:
         echo "please enter right day";
 }
+?>
+<!-- or we can write this way also -->
+<?php
+$weekday = 6;
+switch ($weekday):
+    case 1:
+        echo "today is monday";
+        break;
+    case 2:
+        echo "today is tuesday";
+        break;
+    case 3:
+        echo "today is wednesday";
+        break;
+    case 4:
+        echo "today is thursday";
+        break;
+    case 5:
+        echo "today is friday";
+        break;
+    case 6:
+        echo "today is saturday";
+        break;
+    case 7:
+        echo "today is sunday";
+        break;
+    default:
+        echo "please enter right day";
+endswitch;
+?>
